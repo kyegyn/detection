d9f67e86a9ab37b6f6895c8cc489d55c90a8400e
zhh <1739475669@qq.com>
2026-01-05 17:46:49 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,16 @@
+import random
+import os
+import numpy as np
+import torch
+
+
+
+def seed_torch(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
